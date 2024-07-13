5ac7dfc22b1ce4f3b999044917af258d7dd19703
Chedi <94387435+Chlayem@users.noreply.github.com>
2024-07-13 12:31:55 +0100

    Delete Dashboard directory

--- Dashboard/lib/employee.dart
+++
@@ -1,9 +0,0 @@
-
-class Employee {
-  String name ;
-  String department ;
-  int stress;
-  int anxiety;
-  int fatigue;
-  Employee({required this.name,required this.department,required this.stress,required this.anxiety,required this.fatigue});
-}
